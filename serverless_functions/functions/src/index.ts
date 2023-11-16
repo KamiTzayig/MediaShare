4ca2bd7f868364a0a06118c4290bc7e143d7e9ba
KamiTzayig <Dyekozz@gmail.com>
2023-11-16 08:33:57 +0200

    serverless functions on delete post

--- serverless_functions/functions/src/index.ts
+++ serverless_functions/functions/src/index.ts
@@ -6,7 +6,6 @@ const functions = require('firebase-functions/v1');
 
 admin.initializeApp();
 
-// initializeApp();
 
 exports.onCreateUser = functions.auth.user().onCreate((user: any) => {
     admin.firestore().collection('users').doc(user.uid).set({
@@ -15,3 +14,34 @@ exports.onCreateUser = functions.auth.user().onCreate((user: any) => {
         photoURL: '',       
     });
 });
+
+function getPathStorageFromUrl(url:String){
+
+    const baseUrl = "https://firebasestorage.googleapis.com/v0/b/mediashare-cd7fc.appspot.com/o/";
+
+    let mediaPath:string = url.replace(baseUrl,"");
+
+    const indexOfEndPath = mediaPath.indexOf("?");
+
+   mediaPath =mediaPath.substring(0,indexOfEndPath);
+    
+    mediaPath =mediaPath.replace(/%2F/g,"/");
+    
+ 
+    return mediaPath;
+}
+
+//on post delete
+exports.onPostDelete = functions.firestore.document('posts/{postId}').onDelete(async(snap:any, context:any) => {
+    const postData = snap.data();
+    const mediaDownloadUrl = postData.mediaUrl;
+    const thumbnailDownloadUrl = postData.thumbnailUrl;
+    const mediaPath = getPathStorageFromUrl(mediaDownloadUrl);
+    const thumbnailPath = getPathStorageFromUrl(thumbnailDownloadUrl);
+
+    const bucket = admin.storage().bucket();
+    await bucket.file(thumbnailPath).delete();
+    return bucket.file(mediaPath).delete();
+
+});
+
